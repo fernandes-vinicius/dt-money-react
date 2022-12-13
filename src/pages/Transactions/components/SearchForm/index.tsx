d38ee61665c398f820c5d3de4d5638e4deae068c
vinicius-evocorp <vinicius@evocorp.com.br>
2022-12-13 11:56:42 -0300

    First commit

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,48 @@
+import { useContextSelector } from 'use-context-selector'
+import { useForm } from 'react-hook-form'
+import { MagnifyingGlass } from 'phosphor-react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import { TransactionContext } from '../../../../contexts/TransactionContext'
+
+import { SearchFormContainer } from './styles'
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+  const fetchTransactions = useContextSelector(
+    TransactionContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({ resolver: zodResolver(searchFormSchema) })
+
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} /> Buscar
+      </button>
+    </SearchFormContainer>
+  )
+}
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,48 @@
+import { useContextSelector } from 'use-context-selector'
+
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import { TransactionContext } from '../../contexts/TransactionContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
+
+export function Transactions() {
+  const transactions = useContextSelector(TransactionContext, (context) => {
+    return context.transactions
+  })
+
+  return (
+    <TransactionsContainer>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
+                  </PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+              </tr>
+            ))}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </TransactionsContainer>
+  )
+}
